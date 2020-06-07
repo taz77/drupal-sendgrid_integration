0fe053027dc2a60c4481b7b20d2779abdf172a08
e.bogatyrev <e.bogatyrev@2760527.no-reply.drupal.org>
2020-06-07 09:09:42 -0400

    Issue #3052012 by e.bogatyrev, ashishdalvi, jigish.addweb, waverate: Drupal 9 Deprecated Code Report

--- modules/sendgrid_integration_reports/js/sendgrid_integration_reports.js
+++ modules/sendgrid_integration_reports/js/sendgrid_integration_reports.js
@@ -1,5 +1,6 @@
 /**
- * Sendgrid Reports JS for creating graphs using Google Chart JS API
+ * @file
+ * Sendgrid Reports JS for creating graphs using Google Chart JS API.
  */
 
 (function ($) {
@@ -11,18 +12,18 @@
       });
 
       function drawCharts() {
-        var dataTableVol = new google.visualization.DataTable();
+        const dataTableVol = new google.visualization.DataTable();
         dataTableVol.addColumn('datetime', Drupal.t('Date'));
         dataTableVol.addColumn('number', Drupal.t('Opens'));
         dataTableVol.addColumn('number', Drupal.t('Clicks'));
         dataTableVol.addColumn('number', Drupal.t('Delivered'));
 
-        var dataTableSpa = new google.visualization.DataTable();
+        const dataTableSpa = new google.visualization.DataTable();
         dataTableSpa.addColumn('datetime', Drupal.t('Date'));
         dataTableSpa.addColumn('number', Drupal.t('Spam'));
         dataTableSpa.addColumn('number', Drupal.t('Spam Drops'));
 
-        for (var key in settings.sendgrid_integration_reports.global) {
+        for (let key in settings.sendgrid_integration_reports.global) {
           dataTableVol.addRow([
             new Date(settings.sendgrid_integration_reports.global[key]['date']),
             settings.sendgrid_integration_reports.global[key]['opens'],
@@ -36,16 +37,17 @@
           ]);
         }
 
-        var options = {
+        const options = {
           pointSize: 5,
-          hAxis: {format: 'y/MM/dd'},
-          legend: {position: 'bottom'}
+          hAxis: {format: 'MM/dd/yyyy'},
+          legend: {position: 'bottom'},
+          vAxis: {viewWindowMode: "explicit", viewWindow:{ min: 0 }}
         };
 
-        var chart0 = new google.visualization.LineChart(document.getElementById('sendgrid-global-volume-chart'));
+        const chart0 = new google.visualization.LineChart(document.getElementById('sendgrid-global-volume-chart'));
         chart0.draw(dataTableVol, options);
 
-        var chart1 = new google.visualization.LineChart(document.getElementById('sendgrid-global-spam-chart'));
+        const chart1 = new google.visualization.LineChart(document.getElementById('sendgrid-global-spam-chart'));
         chart1.draw(dataTableSpa, options);
       }
     }
